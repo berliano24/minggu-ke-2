a18a55fca2f85659d6f758e3da06a44e03110853
berliano24 <113021520+berliano24@users.noreply.github.com>
2023-01-13 13:38:50 +0700

    Delete pbw-main directory

--- pbw-main/main.js
+++
@@ -1,9 +0,0 @@
-function hamburgerMenu() {
-    var x = document.querySelector('.dropdown-content-menu');
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-
-    }
-}
